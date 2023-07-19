f82a2436bc92f96cdfa77b6fd9ebd9c159f67355
harsh mittal <harshmittal1750@gmail.com>
2023-07-20 02:05:30 +0530

    connectkit with siwe

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    return config;
+  },
+};
+module.exports = nextConfig;
---
+++ src/lib/consts.ts
@@ -0,0 +1,2 @@
+export const APP_NAME = "Parametrica";
+export const COOKIE_NAME = "web3session";
