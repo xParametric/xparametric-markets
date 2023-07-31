9b85f9cdfc5e06fb783e7db274099056cdc031c9
harsh mittal <harshmittal1750@gmail.com>
2023-08-01 04:02:01 +0530

    carosual cards

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,9 +2,15 @@
 import { useEffect } from "react";
 import dbConnect from "@/config/dbConfig";
 import MarketCard from "@/components/MarketCard";
+import CarousalCards from "@/components/LandingPageCarousal";
+import LandingPageCarousal from "@/components/LandingPageCarousal";
 
 const Home = () => {
-  return <main className="background"></main>;
+  return (
+    <main className="background">
+      <LandingPageCarousal />
+    </main>
+  );
 };
 
 export default Home;
