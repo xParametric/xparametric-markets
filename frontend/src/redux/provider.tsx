e33b8a04cda290c1c658729b7045df9b63ee41b1
harsh mittal <harshmittal1750@gmail.com>
2023-08-04 06:17:37 +0530

    build fix

---
+++ frontend/src/redux/provider.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { Provider } from "react-redux";
+import store from "./store";
+import React, { ReactNode } from "react";
+export function ReduxProvider({ children }: { children: ReactNode }) {
+  return <Provider store={store}>{children}</Provider>;
+}
